b77eef4d1b7950e1d09a575af4aa7cf41a474a6e
Yeneli GM <uchyeneli@gmail.com>
2023-02-09 13:10:08 -0600

    Configuring  Server & adding test route

--- api-social-network/index.js
+++ api-social-network/index.js
@@ -1,18 +1,39 @@
 //Import dependencies
 const { connect } = require('./database/connection')
+const express = require('express')
+const cors = require('cors')
 
-//mesagge
-console.log("Social network api running");
 
 //Conexion a bd
 connect();
 
 //Create node server
-
-//Conf cors
+const app = express();
+const port = 3900
 
 /// Convert body data to json objects
+app.use(cors())//Conf cors -middleware (Executes before the routes / endpoints ) -middleware
+app.use(express.json()) //Decodes body data to json format-middleware
+app.use(express.urlencoded({ extended: true })) //Decodes data with form-urlencoded -middleware
 
-//Load routes conf
+
+//Load routes conf (pending)
+
+//Test route
+app.get("/test-route", (req, res) => {
+
+    return res.status(200).json(
+        {
+            id: 3,
+            content: 'sup testing',
+            more: req.body
+        }
+    )
+})
+
 
 //Get the server to listen to petitions
+app.listen(port, () => {
+    //mesagge
+    console.log("Server running on port " + port);
+})
