bf6a0b7d674fae3607e2bb5c08bc6f3ce0d04cd8
Yeneli GM <uchyeneli@gmail.com>
2023-02-18 11:54:53 -0600

    Finishing timeline

--- front-social-network/src/components/post/Feed.js
+++ front-social-network/src/components/post/Feed.js
@@ -1,164 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState, useLayoutEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
 import avatar from '../../assets/img/user.png'
+import { GetProfile } from '../../helpers/GetProfile'
+import { Global } from '../../helpers/Global'
 import useAuth from '../../hooks/useAuth'
+import { PostList } from '../post/PostList'
+
 
 const Feed = () => {
+
+    // const [profile, setProfile] = useState({})
+    const params = useParams();
+    // const [counters, setCounters] = useState({})
+    // const [following, setFollowing] = useState(false)
+    const [posts, setPosts] = useState([])
+    const [postsPage, setPostsPage] = useState(1)
+    const [showMore, setShowMore] = useState(true)
+
+    const { auth } = useAuth();
+
+
+    useEffect(() => {
+        setPostsPage(1)
+        // getDataUser()
+        // getCounters()
+
+    }, [])
+
+    // useEffect(() => {
+    //     setPostsPage(1)
+    //     getDataUser()
+    //     getCounters()
+
+    // }, [params])
+
+    useEffect(() => {
+        getPosts()
+    }, [postsPage])
+
+
+    const getPosts = async () => {
+        console.log('--------- ' + postsPage);
+
+        const request = await fetch(Global.url + 'post/feed-posts/' + postsPage, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": localStorage.getItem('sessionToken')
+            }
+        })
+        const data = await request.json()
+
+        console.log(data);
+        if (data.status == 'success') {
+
+            if (postsPage == 1) {
+                setPosts(data.postPaginated)
+
+            } else {
+                setPosts([...posts, ...data.postPaginated])
+            }
+        }
+
+        if (postsPage == data.numberOfPages || data.numberOfPages == 1) {
+            setShowMore(false)
+        } else {
+            setShowMore(true)
+        }
+    }
 
     return (
         <>
             <header className="content__header">
                 <h1 className="content__title">Timeline</h1>
-                <button className="content__button">Mostrar nuevas</button>
+                <button className="content__button" onClick={() => { setPostsPage(1) }}> Show newest</button>
             </header>
+            <PostList posts={posts} setPostsPage={setPostsPage} postsPage={postsPage} showMore={showMore} />
 
-            <div className="content__posts">
-
-                <div className="posts__post">
-
-                    <div className="post__container">
-
-                        <div className="post__image-user">
-                            <a href="#" className="post__image-link">
-                                <img src={avatar} className="post__user-image" alt="Foto de perfil" />
-                            </a>
-                        </div>
-
-                        <div className="post__body">
-
-                            <div className="post__user-info">
-                                <a href="#" className="user-info__name">Victor Robles</a>
-                                <span className="user-info__divider"> | </span>
-                                <a href="#" className="user-info__create-date">Hace 1 hora</a>
-                            </div>
-
-                            <h4 className="post__content">Hola, buenos dias.</h4>
-
-                        </div>
-
-                    </div>
-
-
-                    <div className="post__buttons">
-
-                        <a href="#" className="post__button">
-                            <i className="fa-solid fa-trash-can"></i>
-                        </a>
-
-                    </div>
-
-                </div>
-
-                <div className="posts__post">
-
-                    <div className="post__container">
-
-                        <div className="post__image-user">
-                            <a href="#" className="post__image-link">
-                                <img src={avatar} className="post__user-image" alt="Foto de perfil" />
-                            </a>
-                        </div>
-
-                        <div className="post__body">
-
-                            <div className="post__user-info">
-                                <a href="#" className="user-info__name">Victor Robles</a>
-                                <span className="user-info__divider"> | </span>
-                                <a href="#" className="user-info__create-date">Hace 1 hora</a>
-                            </div>
-
-                            <h4 className="post__content">Hola, buenos dias.</h4>
-
-                        </div>
-                    </div>
-
-                    <div className="post__buttons">
-
-                        <a href="#" className="post__button">
-                            <i className="fa-solid fa-trash-can"></i>
-                        </a>
-
-                    </div>
-
-                </div>
-
-
-                <div className="posts__post">
-
-                    <div className="post__container">
-
-                        <div className="post__image-user">
-                            <a href="#" className="post__image-link">
-                                <img src={avatar} className="post__user-image" alt="Foto de perfil" />
-                            </a>
-                        </div>
-
-                        <div className="post__body">
-
-                            <div className="post__user-info">
-                                <a href="#" className="user-info__name">Victor Robles</a>
-                                <span className="user-info__divider"> | </span>
-                                <a href="#" className="user-info__create-date">Hace 1 hora</a>
-                            </div>
-
-                            <h4 className="post__content">Hola, buenos dias.</h4>
-
-                        </div>
-                    </div>
-
-                    <div className="post__buttons">
-
-                        <a href="#" className="post__button">
-                            <i className="fa-solid fa-trash-can"></i>
-                        </a>
-
-                    </div>
-
-                </div>
-
-
-
-
-                <div className="posts__post">
-
-                    <div className="post__container">
-
-                        <div className="post__image-user">
-                            <a href="#" className="post__image-link">
-                                <img src={avatar} className="post__user-image" alt="Foto de perfil" />
-                            </a>
-                        </div>
-
-                        <div className="post__body">
-
-                            <div className="post__user-info">
-                                <a href="#" className="user-info__name">Victor Robles</a>
-                                <span className="user-info__divider"> | </span>
-                                <a href="#" className="user-info__create-date">Hace 1 hora</a>
-                            </div>
-
-                            <h4 className="post__content">Hola, buenos dias.</h4>
-
-                        </div>
-                    </div>
-
-                    <div className="post__buttons">
-
-                        <a href="#" className="post__button">
-                            <i className="fa-solid fa-trash-can"></i>
-                        </a>
-
-                    </div>
-
-                </div>
-
-
-            </div>
-
-            <div className="content__container-btn">
-                <button className="content__btn-more-post">
-                    Ver mas publicaciones
-                </button>
-            </div>
         </>
     )
 }
--- front-social-network/src/components/user/Profile.js
+++ front-social-network/src/components/user/Profile.js
@@ -4,6 +4,7 @@ import avatar from '../../assets/img/user.png'
 import { GetProfile } from '../../helpers/GetProfile'
 import { Global } from '../../helpers/Global'
 import useAuth from '../../hooks/useAuth'
+import { PostList } from '../post/PostList'
 export const Profile = () => {
 
     const [profile, setProfile] = useState({})
@@ -56,9 +57,9 @@ export const Profile = () => {
         getPosts()
     }, [postsPage])
 
-    const nextPage = () => {
-        setPostsPage(postsPage + 1)
-    }
+    // const nextPage = () => {
+    //     setPostsPage(postsPage + 1)
+    // }
 
     const getDataUser = async () => {
         let dataProfile = await GetProfile(params.idToConsult, setProfile)
@@ -142,23 +143,23 @@ export const Profile = () => {
         }
     }
 
-    const deletePost = async (postId) => {
-        const request = await fetch(Global.url + 'post/remove/' + postId, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": localStorage.getItem('sessionToken')
-            }
-        })
+    // const deletePost = async (postId) => {
+    //     const request = await fetch(Global.url + 'post/remove/' + postId, {
+    //         method: "DELETE",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             "Authorization": localStorage.getItem('sessionToken')
+    //         }
+    //     })
 
-        const data = await request.json();
-        console.log(data);
-        if (data.status == 'success') {
-            let filteredList = posts.filter(post => post._id !== postId)
-            // setPosts(filteredList)
-            setPostsPage(1)
-        }
-    }
+    //     const data = await request.json();
+    //     console.log(data);
+    //     if (data.status == 'success') {
+    //         let filteredList = posts.filter(post => post._id !== postId)
+    //         // setPosts(filteredList)
+    //         setPostsPage(1)
+    //     }
+    // }
 
     return (
         <>
@@ -223,61 +224,8 @@ export const Profile = () => {
                 </div>
             </div>
 
-            <div className="content__posts">
-                {posts.map(post => {
-                    return (
-                        <article className="posts__post" key={post._id}>
+            <PostList posts={posts} setPostsPage={setPostsPage} postsPage={postsPage} showMore={showMore} />
 
-                            <div className="post__container">
-
-                                <div className="post__image-user">
-                                    <Link to={"/social/profile/" + post.user._id} className="post__image-link">
-                                        {post.user.image === 'default.png' ?
-                                            <img src={avatar} className="post__user-image" alt="Foto de perfil" />
-                                            : <img src={Global.url + 'user/show-avatar/' + post.user.image} className="post__user-image" alt="Foto de perfil" />
-
-                                        }
-
-                                    </Link>
-                                </div>
-
-                                <div className="post__body">
-
-                                    <div className="post__user-info">
-                                        <Link to={"/social/profile/" + post.user._id} className="user-info__name">{post.user.name} {post.user.surname}</Link>
-                                        <span className="user-info__divider"> | </span>
-                                        <a href="#" className="user-info__create-date">{post.created_at}</a>
-                                    </div>
-
-                                    <h4 className="post__content">{post.text}</h4>
-                                    {post.file && <img src={Global.url + 'post/post-image/' + post.file} />}
-                                </div>
-                            </div>
-
-                            <div className="post__buttons">
-                                {auth._id == post.user._id &&
-                                    <button onClick={() => { deletePost(post._id) }} className="post__button delete-button">
-                                        <i className="fa-solid fa-trash-can"></i>
-                                    </button>
-                                }
-
-
-                            </div>
-
-                        </article>
-                    )
-                })}
-
-            </div>
-
-            <div className="content__container-btn">
-                {showMore ?
-                    <button onClick={nextPage} className="content__btn-more-post">
-                        Ver mas publicaciones
-                    </button> : <strong>No more posts to show...</strong>}
-
-            </div>
-            <br />
         </>
     )
 }
