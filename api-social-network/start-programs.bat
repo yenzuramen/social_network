4af1834c35b062984403fb4a8346e442be814ea1
Yeneli Garcia M <98723864+yenzuramen@users.noreply.github.com>
2023-02-25 15:30:35 -0600

    Delete start-programs.bat

--- api-social-network/start-programs.bat
+++
@@ -1,2 +0,0 @@
-start cmd.exe /k "cd C:\Program Files\MongoDB\Server\5.0\bin && mongod.exe"
-start cmd.exe /k "cd C:\Users\Yeneli\Desktop\Cursos\React Master\final-project\api-social-network && npm start"
